905a0184e175947668b37d7d011e8b9e4b1c942b
MoonSeoSDEV220 <mseo7@ivytech.edu>
2025-01-31 01:59:08 -0500

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,29 @@
+# %%
+#3.1
+
+seconds_in_minute: int = 60
+minutes_in_hour: int = 60
+print("Seconds in an hour", seconds_in_minute * minutes_in_hour)
+
+# %%
+#3.2
+seconds_per_hour: int = 3600
+
+# %%
+#3.3
+print("Seconds in a day", seconds_per_hour * 24)
+
+# %%
+#3.4
+seconds_per_day: int = seconds_per_hour * 24
+
+# %%
+#3.5
+seconds_per_day / seconds_per_hour
+
+# %%
+#3.6
+# Yes, the number is the same except the floating-point.
+seconds_per_day // seconds_per_hour
+
+
